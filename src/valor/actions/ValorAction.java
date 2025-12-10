dd177f0698917ef40abbdf083d6e14b58237ba95
Saksham Goel <saksham0902goel@gmail.com>
2025-12-09 23:00:59 -0500

    added valor pdf

---
+++ src/valor/actions/ValorAction.java
@@ -0,0 +1,15 @@
+package valor.actions;
+
+// Actions a hero can take in Legends of Valor
+public enum ValorAction {
+    MOVE,
+    ATTACK,
+    CAST_SPELL,
+    TELEPORT,
+    RECALL,
+    EQUIP,
+    USE_POTION,
+    PASS
+}
+
+
